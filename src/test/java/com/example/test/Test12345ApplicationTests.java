f9992d00820f7f2838a1e3054d31f8bf09cdf10c
pathania0007 <160423986+pathania0007@users.noreply.github.com>
2024-04-11 03:00:30 -0400

    initial commit

---
+++ src/test/java/com/example/test/Test12345ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test12345ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
